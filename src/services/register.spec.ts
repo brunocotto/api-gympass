947affbf877a5644a21842aab6bd43fad6409524
brunocotto <bruno.ottoo@gmail.com>
2023-03-16 22:25:05 -0300

    test registration attempt with existing user

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,3 +1,4 @@
+import { UserAlreadyExistsError } from './../errors/user-already-exists'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { compare } from 'bcryptjs'
 import { expect, describe, it } from 'vitest'
@@ -20,5 +21,30 @@ describe('Register use case', () => {
     )
 
     expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+})
+describe('should not be able to register with same email twice', () => {
+  it('should hash user password upon registration', async () => {
+    const UsersRepository = new InMemoryUsersRepository()
+    const registeUseCase = new RegisterUseCase(UsersRepository)
+
+    const email = 'johndoe@example.com'
+
+    await registeUseCase.execute({
+      name: 'John Doe',
+      email,
+      password: '123456',
+    })
+
+    expect(
+      () =>
+        registeUseCase.execute({
+          name: 'John Doe',
+          email,
+          password: '123456',
+        }),
+      // esperando um erro da promise Reject
+      // e que o erro seja uma instância de UserAlreadyExistsError
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
   })
 })
